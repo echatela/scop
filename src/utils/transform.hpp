dd06cc2ebd8781002e43c88cc84ac54a533162c2
echatela <ed_srv@proton.me>
2026-08-12 06:19:55 +0200

    transformations done:

--- src/utils/transform.hpp
+++ src/utils/transform.hpp
@@ -2,6 +2,7 @@
 
 #include "mat4.hpp"
 #include "utils/vec3.hpp"
+#include <cmath>
 
 namespace scm
 {
@@ -31,9 +32,47 @@ inline Mat4 translate(const Mat4& m, const Vec3& v)
 
 inline Mat4 rotateX(const Mat4& m, float angle)
 {
+	const float c = std::cos(angle);
+	const float s = std::sin(angle);
+
 	Mat4 r = m;
 
-	r.m[0][1] =
+	for (int row = 0; row < 4; ++row)
+	{
+		r.m[1][row] = m.m[1][row] * c + m.m[2][row] * s;
+		r.m[2][row] = m.m[1][row] * -s + m.m[2][row] * c;
+	}
+	return r;
+}
+
+inline Mat4 rotateY(const Mat4& m, float angle)
+{
+	const float c = std::cos(angle);
+	const float s = std::sin(angle);
+
+	Mat4 r = m;
+
+	for (int row = 0; row < 4; ++row)
+	{
+		r.m[2][row] = m.m[2][row] * c + m.m[0][row] * s;
+		r.m[0][row] = m.m[2][row] * -s + m.m[0][row] * c;
+	}
+	return r;
+}
+
+inline Mat4 rotateZ(const Mat4& m, float angle)
+{
+	const float c = std::cos(angle);
+	const float s = std::sin(angle);
+
+	Mat4 r = m;
+
+	for (int row = 0; row < 4; ++row)
+	{
+		r.m[0][row] = m.m[0][row] * c + m.m[1][row] * s;
+		r.m[1][row] = m.m[0][row] * -s + m.m[1][row] * c;
+	}
+	return r;
 }
 
 } // namespace scm
